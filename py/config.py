af06d62ca5138998adf2c7244777de78a60d4a5d
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-11-11 20:22:03 -0800

    huh

---
+++ py/config.py
@@ -0,0 +1,8 @@
+
+
+# The email for your account (change it to your email)
+email = "juderlunez@hotmail.com"
+# The password for your account (change it to your password) 
+password = "pwzh3uNwtPEX"
+# How many tokens to generate (change to specify how many tokens you want)
+amount = 1
